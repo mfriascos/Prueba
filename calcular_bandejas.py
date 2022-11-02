363969b9a60726670acb95ea3209a0f9d318e17f
Mario <marioriascos1201@gmail.com>
2022-11-01 20:14:15 -0500

    Bug Repair

--- calcular_bandejas.py
+++ calcular_bandejas.py
@@ -1,5 +1,7 @@
+import math
+
 def calcular_bandejas(numero_huevos,huevos_por_bandejas):
     
-    return numero_huevos//huevos_por_bandejas
+    return math.ceil(numero_huevos/huevos_por_bandejas)
    
     
